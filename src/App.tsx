fdd8b8a79252d485d078cade2ddbf8ede24fb80d
Xin Qiao <qiaoxin136@gmail.com>
2025-06-24 09:24:26 -0400

    allow multiple users

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 function App() {
 
-    const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -27,14 +27,14 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-            <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
